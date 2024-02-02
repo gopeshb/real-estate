2188811d82210b5f0e2477de4523dc15f5d4e381
gopeshb <gopeshbhatia01@outlook.com>
2024-02-02 17:07:07 +0530

    text route created successfully

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from "./routes/user.route.js"
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('Connected to database successfully');
+}).catch((error)=>{
+console.log(error)
+});
 const app=express();
-const PORT=3000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`server is running at PORT no. ${PORT}`)
 });
+app.get('/',(req,res)=>{
+    res.send("hello from backend");
+})
+app.use("/api/user",userRouter);
