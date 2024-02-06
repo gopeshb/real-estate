5baffa409759a0860c3135b2bded93420e9ae9f1
gopeshb <gopeshbhatia01@outlook.com>
2024-02-07 00:23:10 +0530

    update user routes created successfully

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,28 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
 export const test=(req,res)=>{
     res.send("dhat teri maa ki....")
+}
+export const updateUser= async (req,res,next)=>{
+    if (req.user.id !== req.params.id) 
+        return next(errorHandler(401, 'Unauthorized: You do not have permission to update this user.'));
+    try {
+        if(req.body.password){
+            req.body.password=bcryptjs.hashSync(req.body.password,10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:{
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                avatar:req.body.avatar,
+            }
+        },{new: true})
+        const {password:pass,...rest}=updatedUser;
+        res.status(200).json({ message: 'User updated successfully.', data: rest });
+    } catch (error) {
+        next(error);
+    }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log('Connected to database successfully');
@@ -11,6 +12,7 @@ console.log(error)
 });
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`server is running at PORT no. ${PORT}`)
