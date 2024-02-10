a4e345759989a9ebf069c41c21e3b683674a09ee
gopeshb <gopeshbhatia01@outlook.com>
2024-02-10 15:58:42 +0530

    user listings api route created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import Listing from "../models/listing.model.js";
 
 export const test=(req,res)=>{
     res.send("dhat teri maa ki....")
@@ -37,3 +38,15 @@ export const deleteUser = async (req, res, next) => {
       next(error);
     }
   };
+  export const getUserListing=async (req,res,next)=>{
+    if (req.user.id === req.params.id) {
+        try {
+          const listings = await Listing.find({ userRef: req.params.id });
+          res.status(200).json(listings);
+        } catch (error) {
+          next(error);
+        }
+      } else {
+        return next(errorHandler(401, 'You are not verified to view this listing'));
+      }
+  }
