0fec1063f2e33520fb034419c25a96a1dc666e3b
gopeshb <gopeshbhatia01@outlook.com>
2024-02-14 22:32:11 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,15 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log('Connected to database successfully');
 }).catch((error)=>{
 console.log(error)
 });
+const __dirname=path.resolve();
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +27,11 @@ app.get('/',(req,res)=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','intex.html'));
+})
+
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
     const message=err.message||'Internal Server Error';
