079dbb2fb0d4f652e3d3a33a408849af5f8b8727
gopeshb <gopeshbhatia01@outlook.com>
2024-02-18 20:04:24 +0530

    ui wapis se change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -42,11 +42,11 @@ export default function Home() {
   }, []);
   return (
     <div className='w-full flex flex-col mx-auto bg-[#0d192b]'>
-      <div className='title-container max-w-3xl mx-auto items-center object-contain invert rounded-full drop-shadow-xl hue-rotate-50 shadow-lg 
+      <div className='title-container max-w-3xl mt-10 mx-auto items-center object-cover invert rounded-full drop-shadow-xl hue-rotate-50 shadow-lg 
       shadow-blue-800 flex flex-col flex-wrap justify-center w-full text-center h-[440px] '
        style={{ backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/real-estate-315ad.appspot.com/o/background-img.jpg?alt=media&token=ce7b97ae-f405-4f9e-b51a-e81db70524ac)' }}>
-        <p className='p-3 m-2 rounded-lg title-left text-3xl font-bold text-gray-300 bg-gradient-to-r  from-green-400 to-blue-500'>Are You Looking For A New Home?</p>
-        <p className='title-right text-3xl rounded-lg text-teal-900 bg-gradient-to-r from-sky-500 to-blue-500 p-3 font-bold'>Try 
+        <p className='p-3 m-2 rounded-lg title-left text-2xl sm:text-3xl font-bold text-gray-300 bg-gradient-to-r  from-green-400 to-blue-500'>Are You Looking For A New Home?</p>
+        <p className='title-right text-2xl sm:text-3xl rounded-lg text-teal-900 bg-gradient-to-r from-sky-500 to-blue-500 p-3 font-bold'>Try 
         <span className='text-emerald-200 '> Property</span>
         <span className='text-indigo-700'>Estate</span></p>
       </div>
